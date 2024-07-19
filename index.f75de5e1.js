7205c6d2f9077e3625f5d2a8ccde72203634cefb
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-20 00:04:34 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const ul = document.querySelector("ul");
+const liElement = [
+    ...document.querySelectorAll("li")
+];
+function parseSalary(salaryStr) {
+    return +salaryStr.slice(1).split(",").join("");
+}
+function sortEmployeesBySalary(list) {
+    list.sort((a, b)=>parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary));
+    ul.append(...list);
+}
+function employeeList(list) {
+    return list.map((li)=>({
+            name: li.innerText,
+            ...li.dataset
+        }));
+}
+sortEmployeesBySalary(liElement);
+employeeList(liElement);
+
+//# sourceMappingURL=index.f75de5e1.js.map
